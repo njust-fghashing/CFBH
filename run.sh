10008ebbc5786b0ab784395279f7a83be86407a2
njust-fghashing <18868629396@qq.com>
2024-08-03 18:06:28 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-
+##cub_bird aircraft stanford_car nabirds food101 vegfru##
 conda activate base
 python CFBH.py --dataset cub_bird --num_parts 64 
 python CFBH.py --dataset food101  --num_parts 128 --batch_size 256
